70640f3aa5b98284a26d491cdaefb784b3ca71fe
mireki19 <mireki174@gmail.com>
2021-12-02 20:34:08 +0100

    init

---
+++ src/main/java/MavenMain.java
@@ -0,0 +1,11 @@
+public class MavenMain {
+
+    public static void main(String[] args) {
+        System.out.println("test lekce sedm");
+    }
+    public int sum(int a , int b){
+        return a+b;
+    }
+
+
+}
